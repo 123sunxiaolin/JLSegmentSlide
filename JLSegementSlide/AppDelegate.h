cd3566cea2192db62562dd01ec0d98a61fc71391
Jacklin <401788217@qq.com>
2020-03-09 17:51:57 +0800

    feat: initial project

---
+++ JLSegementSlide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JLSegementSlide
+//
+//  Created by Jacklin on 2020/3/9.
+//  Copyright © 2020 Jacklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JLSegementSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JLSegementSlide
+//
+//  Created by Jacklin on 2020/3/9.
+//  Copyright © 2020 Jacklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
